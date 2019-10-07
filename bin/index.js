c7f69a8c0f24c689dcb777dc04dcb5179d8b6ab5
stephenyu <stephen@canva.com>
2019-10-08 07:12:15 +1100

    Removed deadcode. Cleaned up a bit

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,9 @@
 const fs = require('fs');
 const chalk = require('chalk');
 const { exec } = require('child_process');
+const readline = require('readline');
+
+const configurationFileLocation = "./.configuration";
 
 function displayPRs(PRs, longestPRNumber) {
     const displayNumber = (number) => {
@@ -25,23 +28,26 @@ function getUserName(token) {
     return new Promise((resolve, reject) => {
         const cmd = `curl -H "Authorization: token ${token}" https://api.github.com/user`;
 
-        exec(cmd, (error, stdout, stderr) => {
+        exec(cmd, (error, stdout) => {
             if (error) {
                 console.error(`Unable to get Github Username: ${error}`);
-                return;
+                reject();
             }
 
             const body = JSON.parse(stdout);
-            return resolve(body.login);
+
+            (body.login)
+                ? resolve(body.login)
+                : reject();
         });
     });
 }
 
 async function getPRs(token, login) {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
         const cmd = `curl -H "Authorization: token ${token}" https://api.github.com/search/issues?q=is%3Apr+is%3Aopen+sort%3Aupdated-desc+review-requested%3A${login}`;
 
-        exec(cmd, (error, stdout, stderr) => {
+        exec(cmd, (error, stdout) => {
             if (error) {
                 console.error(`Unable to get PRs: ${error}`);
                 return;
@@ -54,37 +60,41 @@ async function getPRs(token, login) {
 }
 
 async function createTokenConfig() {
-    const readline = require('readline').createInterface({
+    const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
     console.info('Visit https://github.com/settings/tokens and create a Personal access tokens');
 
-    readline.question('Token: ', async token => {
-        const login = await getUserName(token);
+    rl.question('Token: ', async token => {
+        try {
+            const login = await getUserName(token);
 
-        fs.writeFileSync('./.configuration', JSON.stringify({
-            token, login
-        }), 'utf8');
+            fs.writeFileSync(configurationFileLocation, JSON.stringify({
+                token, login
+            }), 'utf8');
 
-        process.exit();
+            process.exit();
+        } catch {
+            throw new Error('Unable to get Username');
+        }
     });
 }
 
 (async () => {
     try {
-        if (fs.existsSync('./.configuration')) {
-            const { login, token } = JSON.parse(fs.readFileSync('./.configuration', 'utf8'));
+        if (fs.existsSync(configurationFileLocation)) {
+            const { login, token } = JSON.parse(fs.readFileSync(configurationFileLocation, 'utf8'));
 
             const result = await getPRs(token, login);
             const PRs = result.map(prRAW => ({number: prRAW.number, author: prRAW.user.login, title: prRAW.title, labels: []}));
 
             const longestPRNumber = result.reduce((longest, pr) => {
                 const string = ''+pr.number;
-                  return (string.length > longest)
-                      ? string.length
-                : longest;
+                return (string.length > longest)
+                    ? string.length
+                    : longest;
             }, 0);
 
             displayPRs(PRs, longestPRNumber);
@@ -95,15 +105,3 @@ async function createTokenConfig() {
         console.error(err)
     }
 })();
-
-
-
-// const TOKEN = 'e679f4140ce3c9fea176d0dc02034384713306b6';
-
-// exec(cmd, (error, stdout, stderr) => {
-//   if (error) {
-//     console.error(`exec error: ${error}`);
-//     return;
-//   }
-//   console.log(`stdout: ${stdout}`);
-// });
